7b9c17f620b98944a036419f879c2a078deca44a
XieBo <hippeauxie@gmail.com>
2023-01-07 18:54:00 +0800

    chore: fix eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,24 +1,32 @@
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
+require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
   extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/eslint-config-typescript",
-    "@vue/eslint-config-prettier",
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier',
+    '@vue/prettier',
   ],
   overrides: [
     {
-      files: ["cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}"],
-      extends: ["plugin:cypress/recommended"],
+      files: ['cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}'],
+      extends: ['plugin:cypress/recommended'],
     },
   ],
   parserOptions: {
-    ecmaVersion: "latest",
+    ecmaVersion: 'latest',
   },
   rules: {
-    "vue/multi-word-component-names": "off",
+    'vue/multi-word-component-names': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: false,
+      },
+    ],
   },
-};
+}
---
+++ .prettierrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  tabWidth: 2,
+  // 使用 tabs 替代空格
+  useTabs: false,
+  // 行尾 分号
+  semi: false,
+  singleQuote: true,
+  // props 是否用 引号 包裹
+  quoteProps: "as-needed",
+  // 单引号
+  jsxSingleQuote: true,
+  // 行尾逗号
+  trailingComma: "all",
+  // 花括号内填充空格 { a: 23 }
+  bracketSpacing: true,
+  // 换行标识符
+  endOfLine: "lf",
+  // 用于 Markdown 等，自动语言识别
+  embeddedLanguageFormatting: "auto",
+};
